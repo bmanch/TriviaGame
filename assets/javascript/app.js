2f1ed743b2a7d8ad25077d1337e9d52988ff890d
Brian Manchester <brian.l.manchester@gmail.com>
2017-03-21 17:50:50 -0400

    styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,12 @@ var trivia = {
 		"What did John Leland of Cheshire, New England, present to President Thomas Jefferson in 1802 (the item weighed over 1,200 pounds!)?",
 		"In the presidential election of 1912, Theodore Roosevelt ran for a third (though not successive) term as the nominee for which party?",
 		"During which time span did the US receive over 20 million immigrants, many of whom were from Central, Eastern, or Southern Europe?",
-		"Commenting on the end of chattel slavery, who remarked that \"The slave went free; stood a brief moment in the sun; then moved back again toward slavery.\"?",
-		"Later in his career, Billy Graham was fairly outspoken about his concern over what?",
+		"Commenting on the end of chattel slavery, who remarked that \"The slave went free; stood a brief moment in the sun; then moved back again toward slavery\"?",
+		"To what does the \"Cult of Domesticity\" refer?",
 		"In December, 1784, Thomas Jefferson wrote a letter to James Madison in which he says the following regarding Patrick Henry, \"While Mr. Henry lives another bad constitution would be formed, and saddled for ever on us. What we have to do I think is devoutly to pray for his death....\" What is it that causes Thomas Jefferson to make this remark concerning Patrick Henry?",
-		"Horace Bushnell derided what kind of parenting?",
-		"Of whose oratory was Phillis Wheatley so enamored?",
-		"Where and when does Aimee Semple McPherson's ministry take off?"
+		"To what was the \"Massive Resistance\" movement a response?",
+		"Which president was responsible for the suffering of the Cherokee on the \"Trail of Tears\"?",
+		"During which years did the Civil War take place?"
 	],
 
 	questionAnswerBank: [
@@ -18,11 +18,11 @@ var trivia = {
 		"Bull Moose Party",
 		"Between 1880-1920",
 		"W.E.B. DuBois",
-		"Nuclear armament and warfar",
+		"A 19th-century ideology of separate spheres for men and women",
 		"Henry's position on religion and state in Virginia",
-		"Ostrich Parenting",
-		"George Whitefield",
-		"Los Angeles, California, early 1900s"
+		"1950s resistance to school integration (i.e., desegregation) efforts",
+		"Andrew Jackson",
+		"1861-1865"
 	],
 
 	mcBank: [
@@ -31,11 +31,15 @@ var trivia = {
 		["<h4 data-answer='y'>Bull Moose Party</h4>", "<h4>Green Party</h4>", "<h4>Republican Party</h4>", "<h4>Libertarian Party</h4>"],
 		["<h4>Between 1810-1860</h4>", "<h4 data-answer='y'>Between 1880-1920</h4>", "<h4>Between 1940-1970</h4>", "<h4>Between 1980-2010</h4>"],
 		["<h4>Olaudah Equiano</h4>", "<h4>Frederick Douglass</h4>", "<h4 data-answer='y'>W.E.B. DuBois</h4>", "<h4>Malcom X</h4>"],
-		["<h4>Abortion</h4>", "<h4>The Civil Rights Movement</h4>", "<h4>Prayer in schools</h4>", "<h4 data-answer='y'>Nuclear armament and warfar</h4>"],
+		["<h4>A popular radio program of the 1950s</h4>", "<h4>A 20th-century ideology of separate spheres for men and women</h4>", "<h4>A 1960s cult in Northern California</h4>", "<h4 data-answer='y'>A 19th-century ideology of separate spheres for men and women</h4>"],
 		["<h4>Henry's position on the imminent Revolution</h4>", "<h4>Henry's position on religion and state in Pennsylvania</h4>", "<h4>Henry's incessant use of his catchphrase, \"Give me liberty, or give me death!\"</h4>", "<h4 data-answer='y'>Henry's position on religion and state in Virginia</h4>"],
-		["<h4 data-answer='y'>Ostrich Parenting</h4>", "<h4>Helicopter Parenting</h4>", "<h4>Intensive Parenting</h4>", "<h4>Extensive Parenting</h4>"],
-		["<h4>Frederick Douglass</h4>", "<h4 data-answer='y'>George Whitefield</h4>", "<h4>Barack Obama</h4>", "<h4>Donald Trump</h4>"],
-		["<h4>Waco, Texas, early 1900s</h4>", "<h4>Waco, Texas, mid 1900s</h4>", "<h4 data-answer='y'>Los Angeles, California, early 1900s</h4>", "<h4>Los Angeles, California, mid 1900s</h4>"]
+		["<h4 data-answer='y'>1950s resistance to school integration (i.e., desegregation) efforts</h4>", "<h4>1960s resistance to the Vietnam War</h4>", "<h4>1970s resistance to a Woodstock sequel</h4>", "<h4>1980s resistance to the Cold War</h4>"],
+		["<h4>John Quincy Adams</h4>", "<h4 data-answer='y'>Andrew Jackson</h4>", "<h4>Woodrow Wilson</h4>", "<h4>Donald Trump</h4>"],
+		["<h4>1770-1776</h4>", "<h4>1845-1850</h4>", "<h4 data-answer='y'>1861-1865</h4>", "<h4>1914-1918</h4>"]
+	],
+
+	gifBank: [
+		'<iframe src="//giphy.com/embed/YrfCkiGL8TPZ6" width="453.4177215189874" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/civil-war-YrfCkiGL8TPZ6">via GIPHY</a></p>',
 	]
 };
 
@@ -119,6 +123,7 @@ $(document).ready(function() {
 
 	function append() {
 		$('#answerContainer').append('<h4> Correct answer: ' + trivia.questionAnswerBank[randomNumber] + '</h4>');
+		$('#answerContainer').append(trivia.gifBank[randomNumber]);
 		setTimeout(renderQuestion, 5000);
 	}
 
